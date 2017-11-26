af9122790dab8fea7f5fcd5e5ad8bb37e4362a05
tonkotsuboy <t.kano.624@gmail.com>
2017-11-26 21:04:05 +0900

    dynamic import: object 2

--- 171122_dynamic_import/script/main6.js
+++ 171122_dynamic_import/script/main6.js
@@ -1,7 +1,11 @@
 async function main() {
-  const { Sub } = await import('./sub.js');
+  const { Sub, Sub2 } = await import('./sub.js');
+
   const sub = new Sub();
   sub.subMethod();
+
+  const sub2 = new Sub();
+  new Sub2().subMethod2();
 }
 
 main();
