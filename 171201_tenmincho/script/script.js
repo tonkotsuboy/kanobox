c5012c8eb42c33a01296198f43fdfeced925701e
tonkotsuboy <t.kano.624@gmail.com>
2017-12-01 16:05:13 +0900

    uni2

--- 171201_tenmincho/script/script.js
+++ 171201_tenmincho/script/script.js
@@ -3,8 +3,13 @@ showMarten('.marten2', 65327);
 showMarten('.marten3', 65334);
 showMarten('.marten4', 65317);
 
-function showMarten(targetId, uniCode) {
-  document.querySelectorAll(targetId).forEach(element => {
+/**
+ * 指定のセレクタにユニコードから生成した文字を表示する
+ * @param targetSelector
+ * @param uniCode
+ */
+function showMarten(targetSelector, uniCode) {
+  document.querySelectorAll(targetSelector).forEach(element => {
     element.innerText = String.fromCharCode(uniCode);
   });
 }
