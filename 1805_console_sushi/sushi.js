01fd2efb594c289bdcedf76ce899b5fc7522db2b
tonkotsuboy <t.kano.624@gmail.com>
2018-05-11 20:06:42 +0900

    console sushi

---
+++ 1805_console_sushi/sushi.js
@@ -0,0 +1,27 @@
+(async () => {
+  const sushi = "🍣";
+  const maxSushi = 100;
+  const sushiUnitCount = 2;
+  const loopNum = 10;
+
+  let gain = true;
+  let count = 0;
+  let loop = 0;
+
+  // for文は初期化、条件、反復終了時の処理のいずれも省略可能
+  for (; loop < loopNum;) {
+    count += gain ? sushiUnitCount : -sushiUnitCount;
+
+    console.log("".padEnd(count, sushi));
+
+    await new Promise(resolve => setTimeout(() => resolve(), 10));
+
+    if (count >= maxSushi) {
+      gain = false;
+    }
+    if (count <= 0) {
+      gain = true;
+      loop += 1;
+    }
+  }
+})();
