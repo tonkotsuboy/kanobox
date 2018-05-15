256ffbec0ff4b0b407e3b1714ca867c6ebcd7b64
tonkotsuboy <t.kano.624@gmail.com>
2018-05-15 11:38:35 +0900

    autoprefixer: code fix

--- 180515_autoprefixer/1_webpack/webpack.config.js
+++ 180515_autoprefixer/1_webpack/webpack.config.js
@@ -1,40 +1,33 @@
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
-        // 対象となるファイルの拡張子
         test: /\.css/,
-        // Sassファイルの読み込みとコンパイル
         use: [
-          // linkタグに出力する機能
-          'style-loader',
-          // CSSをバンドルするための機能
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              // CSS内のurl()メソッドの取り込みを禁止する
               url: false,
-              // CSS+PostCSSの場合は1を指定
               importLoaders: 1
             },
           },
           // PostCSSのための設定
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               plugins: [
+                // postcss-gap-propertiesを有効化
                 require("postcss-gap-properties")(),
                 // Autoprefixerを有効化
                 // ベンダープレフィックスを自動付与する
-                require('autoprefixer')({grid: true})
+                require("autoprefixer")({grid: true})
               ]
-            },
+            }
           }
-        ],
-      },
-    ],
+        ]
+      }
+    ]
   }
 };
--- 180515_autoprefixer/2_gulp/gulpfile.js
+++ 180515_autoprefixer/2_gulp/gulpfile.js
@@ -1,12 +1,11 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-
 const postcssGapProperties = require("postcss-gap-properties");
 
 gulp.task("default", function () {
   return gulp.src("src/style.css")
-    .pipe( postcss([
+    .pipe(postcss([
       postcssGapProperties(),
       autoprefixer({
         grid: true,
